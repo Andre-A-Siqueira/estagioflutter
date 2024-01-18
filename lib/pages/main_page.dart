998966a5b5b1cda59fdc5976f2cf17b7faca8e45
André Siqueira <dev.Andre.Siqueira@gmail.com>
2024-01-18 01:34:22 -0300

    Interacao da tela principal

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,8 @@
-import 'package:estagioflutter/widgets/botao_sidebar.dart';
+import 'package:estagioflutter/pages/dados_cadastrais.dart';
+import 'package:estagioflutter/pages/pagina1.dart';
+import 'package:estagioflutter/pages/pagina2.dart';
+import 'package:estagioflutter/pages/pagina3.dart';
+//import 'package:estagioflutter/widgets/botao_sidebar.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -9,6 +13,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  PageController controller = PageController(initialPage: 0);
+  int posicaoPagina = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,50 +23,57 @@ class _MainPageState extends State<MainPage> {
           title: const Text('Main Page'),
         ),
         drawer: Drawer(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InkWell(
-                child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    width: double.infinity,
-                    child: const Text("Dados Cadastráis")),
-                onTap: () {},
-              ),
-              const Divider(),
-              const SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    width: double.infinity,
-                    child: const Text("Termo de Uso")),
-                onTap: () {},
-              ),
-              const Divider(),
-              const SizedBox(
-                height: 10,
-              ),
-              InkWell(
-                child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    width: double.infinity,
-                    child: const Text("Configuração")),
-                onTap: () {},
-              ),
-              const Divider(),
-              const SizedBox(
-                height: 10,
-              ),
-              //BotaoSideBarTeste00(titulo: "fornecedor"),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Dados Cadastráis")),
+                  onTap: () {
+                    Navigator.pop(context);
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const DadosCadastraisPage(
+                                texto: "Meus dados")));
+                  },
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Termo de Uso e privacidade")),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Configuração")),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+                //BotaoSideBarTeste00(titulo: "fornecedor"),
 
-              //! <===============================================>
+                //! <===============================================>
 
-            //TODO Fazer implementacão das funcões:
-              /*
+                //TODO Fazer implementacão das funcões:
+                /*
             Cadastro de Produtos
             Controle de Estoque
             Alerta de Estoque Mínimo
@@ -71,9 +84,41 @@ class _MainPageState extends State<MainPage> {
             Escanear Código de Barras
             Segurança e Controle de Acesso
             */
-            ],
+              ],
+            ),
           ),
-        )),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: PageView(
+                  controller: controller,
+                  onPageChanged: (value) {
+                    setState(() {
+                      posicaoPagina = value;
+                    });
+                  },
+                  /*scrollDirection: Axis.vertical,*/
+                  children: const [
+                    Pagina1Page(),
+                    Pagina2Page(),
+                    Pagina3Page(),
+                  ]),
+            ),
+            BottomNavigationBar(
+                onTap: (value) {
+                  controller.jumpToPage(value);;;
+                },
+                currentIndex: posicaoPagina,
+                items: const [
+                  BottomNavigationBarItem(
+                      label: "Pag1", icon: Icon(Icons.home)),
+                  BottomNavigationBarItem(label: "Pag2", icon: Icon(Icons.add)),
+                  BottomNavigationBarItem(
+                      label: "Pag3", icon: Icon(Icons.person)),
+                ])
+          ],
+        ),
       ),
     );
   }
